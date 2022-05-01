993dbcc96aa90a889b3cd544a9f420590d34fd1f
Nilton Freitas <mail@nilton.tech>
2022-04-30 23:39:23 -0300

    Add functions to create elements

--- index.js
+++ index.js
@@ -7,16 +7,32 @@ const accentColor = '#777'
 const addButton = itemTemplateLabel.querySelector('button')
 const ul = document.querySelector('.list ul')
 
+const buildCheckbox = () => {
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    return checkbox
+}
+
+const buildSpan = (value) => {
+    const span = document.createElement('span')
+    span.innerText = value
+    return span
+}
+
+const buildLi = () =>  {
+    const li = document.createElement('li')
+    return li
+}
+
 
 const addItem = () => {
     if(!newItemInput.value) {
         return
     }
-    const li = document.createElement('li')
-    const span = document.createElement('span')
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    span.innerText = newItemInput.value
+    const li = buildLi()
+    console.log(li)
+    const span =  buildSpan(newItemInput.value)
+    const checkbox = buildCheckbox()
     li.appendChild(span)
     ul.prepend(li)
     li.insertBefore(checkbox, span)
